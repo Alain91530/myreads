a1b83db8d622c3481460885058beb7819dc10225
Unknown <alain.cadenat@depannum.com>
2018-04-22 17:43:31 +0200

    feat: book can be assigned a new shelf in search page

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 import RenderBook from './RenderBook';    // eslint-disable-line no-unused-vars
-import {search, getAll} from './BooksAPI';
+import {search, getAll, update} from './BooksAPI';
 
 class SearchBooks extends Component {
   state = {
@@ -9,10 +9,6 @@ class SearchBooks extends Component {
     query: ''
   };
 
-  /*
-   * Get all books according to the query from the data base and update state
-   *   to have it rendered
-   */
 
   componentDidMount() {
     getAll().then((myBooks) => {
@@ -20,6 +16,33 @@ class SearchBooks extends Component {
     });
   }
 
+  updateMyBooks(event) {
+    const bookId=document.activeElement.name;
+    let books=this.state.searchedBooks;
+    let myNewBooks=this.state.myBooks
+    const test =[]
+    console.log(books,bookId)
+    let bookMoved=books.filter((book) => (book.id===bookId));
+    bookMoved.shelf=event
+    myNewBooks.push(bookMoved)
+    console.log(bookMoved[0])
+    update(bookMoved[0], event)
+      .then(() => {console.log('updated');(getAll()
+        .then((test) => {console.log('got books',test);this.setState({test})})
+        .catch(console.log('error',this.state.myBooks)))})
+      .catch(console.log(books))
+    //bookMoved.shelf = event
+    //myNewBooks.push(bookMoved[0])
+    //console.log(this.state.myBooks.indexOf(bookMoved[0]))
+    //this.state.myBooks[)].shelf=event;
+    //this.setState({myBooks: myNewBooks});
+    console.log(this.state.myBooks)
+  }
+
+  /*
+   * Get all books according to the query from the data base and update state
+   *   to have it rendered
+   */
   updateQuery = (query) => {
     let searchResult=[];
     searchResult=[];
@@ -54,6 +77,7 @@ class SearchBooks extends Component {
     let searchedBooks= this.state.searchedBooks;
     searchedBooks=searchedBooks.map((book)=>(
       this.checkIfOwned(book)));
+    console.log('render:',this.state.myBooks)
 
     return (
       <div>
@@ -85,9 +109,27 @@ class SearchBooks extends Component {
             <ul className='book-list'>
               {searchedBooks.map((book)=>(
                 <li key={book.id} className='book'>
-                  <RenderBook
-                    book={book}
-                  />
+                  <div className="book-container">
+
+                    <RenderBook
+                      book={book}
+                    />
+                    {console.log(book.shelf)}
+
+                    <select
+                      name= {`${book.id}`}
+                      className="book-shelf-changer"
+                      onChange={(event) => this.updateMyBooks(event.target.value)}
+                      value={book.shelf}
+                    >
+                      <option value="test" disabled>Move to...</option>
+                      <option className="shelf-choice" value="currentlyReading" label='Currently reading'/>
+                      <option className="shelf-choice" value="wantToRead">Want to Read</option>
+                      <option className="shelf-choice"  value="read">Read</option>
+                      <option className="shelf-choice" value="none">None</option>
+                    </select>
+                  </div>
+
                 </li>
               ))
               }
