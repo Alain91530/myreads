d30719059aff0831f8a01239a029229bc474f835
Unknown <alain.cadenat@depannum.com>
2018-04-24 12:09:23 +0200

    feat: get the book from database

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
+import {get} from './BooksAPI';
 
 class BookDetails extends Component {
 
-  render() {
-      const  bookId = this.props.match.params.number;
-      console.log(bookId)
-      return (
+state = {
+  book: {title: ''},
+  bookId:  this.props.match.params.number
+}
+
+componentDidMount() {
+  console.log(this.state.bookId)
+  get(this.state.bookId).then((book) => {
+    this.setState({book: book});})
+}
+
+render() {
+
+  const bookId = this.props.bookId;
+  const book = this.state.book;
+
+  console.log(bookId,book)
+  return (
     <div className="detailed-desc">
-    Book Details
     </div>
-      )
-    }
+  );
+}
 }
 
 export default BookDetails;
--- src/RenderBook.js
+++ src/RenderBook.js
@@ -19,10 +19,12 @@ class RenderBook extends Component {
     const currentBook = this.props.book;
     return(
       <div className="book-container">
-      <Link
-      to = {`/details/${currentBook.id}`}
-      className="book-details">
+        <div className="book-details">
+          <Link
+            to = {`/details/${currentBook.id}`}
+            >
       View details</Link>
+        </div> 
         {/* use the cover pic as background if the book has one */}
         {currentBook.hasOwnProperty('imageLinks')&&(<div className="book-cover"
           style={{backgroundImage: `url(${currentBook.imageLinks.thumbnail})`
